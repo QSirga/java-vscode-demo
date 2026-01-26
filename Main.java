e92176d25d31785ed30ea403b0f9cb7f3895c238
sirga <140368776+QSirga@users.noreply.github.com>
2026-01-26 15:46:11 -0800

    Update output with 2 new  messages

--- Main.java
+++ Main.java
@@ -3,6 +3,9 @@ public class Main {
         System.out.println("Hello, and welcome to Java!");
         System.out.println("This is how you print sentences to the console.");
         System.out.println("Pretty interesting, right?");
+        
+        System.out.println("I forgot to introduce myself!");
+        System.out.println("My name is Becca, and I'm a freshman at CPP.");
     }
     
 }
