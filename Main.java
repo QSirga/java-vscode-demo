70322c55b8c579b99691123ff10ec8e274df9b3f
sirga <140368776+QSirga@users.noreply.github.com>
2026-01-26 15:33:57 -0800

    Initial Java program and gitignore

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, and welcome to Java!");
+        System.out.println("This is how you print sentences to the console.");
+        System.out.println("Pretty interesting, right?");
+    }
+    
+}
