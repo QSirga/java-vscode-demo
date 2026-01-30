cbd46f8ef47ba42f93eaf7886d35409f5c59767b
sirga <140368776+QSirga@users.noreply.github.com>
2026-01-29 16:19:21 -0800

    Added an extra piece of information about me!

--- Main.java
+++ Main.java
@@ -16,6 +16,9 @@ public class Main {
         System.out.println("I forgot to introduce myself!");
         System.out.println("My name is Becca, and I'm a freshman at CPP.");
         System.out.println("I'm majoring in Computer Science");
+        System.out.print("I am eager to join clubs and participate in research oppurtunities.");
+        
+        
     }
     
 }
